7ec8ddd92058c9536d7c390b07b6a3177cafb264
Ksenia <ksenia.selezneva@pramsystem.com>
2019-01-26 23:50:52 +0200

    lider-triggers

--- Заключение_договора/getContractUID.php
+++ Заключение_договора/getContractUID.php
@@ -5,3 +5,9 @@
  * Date: 1/26/2019
  * Time: 10:45 PM
  */
+$doc = 'VA_DI_Agreement';
+$aTasks = PMFGetUidFromText($doc, 'INP_DOC_TITLE', @@PROCESS, 'en');
+if (count($aTasks) == 0) {
+    throw new Exception("Error: Task '$doc' is not found.");
+}
+@@ContractUID = $aTasks[0];
